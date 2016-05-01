651afc227140352fdb8361efa6371e2355803327
Nebojsa <nebojsa.stanic86@gmail.com>
2016-05-01 11:39:43 +0200

    Initial commit.

---
+++ src/ThreadTestDrive.java
@@ -0,0 +1,12 @@
+
+public class ThreadTestDrive {
+
+	public static void main(String[] args) {
+		Runnable theJob = new MyRunnable();
+		Thread t = new Thread(theJob);
+		t.start();
+		System.out.println("back in the main");
+
+	}
+
+}
